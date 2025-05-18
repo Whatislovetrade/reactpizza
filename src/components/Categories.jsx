60b2ecc080afc2cbfa24768e524defec440f4f87
Vlad Ivannikov <vladmaslow1@gmail.com>
2025-05-18 23:29:22 +0500

    Хуки пошли, во основном работали с передачей свойств из родительского в дочерний компонент, понял немного логику

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,27 @@
-const Categories = ({items}) => {
-    console.log(items);
+import { useState } from "react";
+
+const Categories = ({ items, onClickItem }) => {
+
+    const [activeItem, setActiveItem] = useState(null)
+
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+
     
   return (
     <div className="categories">
         <ul>
-            <li className="active">Все</li>
+              <li
+                  className={activeItem === null ? 'active' : ''}
+                  onClick={() => onSelectItem(null)
+                    } >Все</li>
             {
-                items.map(name => (
-                    <li>{name}</li>
+                items.map((name,index) => (
+                    <li
+                        className={activeItem === index ? 'active' : ''}
+                        onClick={() => onSelectItem(index)}
+                        key={`${name}_${index}`}>{name}</li>
                 ))
             }
         </ul>
